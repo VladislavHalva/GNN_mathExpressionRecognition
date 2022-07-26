66b6e6ec1ce68061a1af09142d19be89022adaa1
Vladislav Halva <54715885+VladislavHalva@users.noreply.github.com>
2022-07-26 15:28:03 +0200

    Delete init.sh

--- init.sh
+++
@@ -1,8 +0,0 @@
-pip install torchvision
-pip install opencv-python
-apt-get install ffmpeg libsm6 libxext6  -y
-pip install tokenizers
-pip install matplotlib
-pip install Shapely
-pip install rapidfuzz
-pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.10.0+cu102.html
